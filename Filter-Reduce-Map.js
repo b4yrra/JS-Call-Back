6f4f1fd71e77519c8ebbf9c06590170821be3954
cb4yrra <t.chinbayr@gmail.com>
2025-12-04 09:45:01 +0800

    cb

--- Filter-Reduce-Map.js
+++ Filter-Reduce-Map.js
@@ -122,3 +122,174 @@ reduceDividThre = dividByThree.reduce((a, b) => {
 console.log("7.", reduceDividThre);
 
 // 8.
+
+// const modulaNums = [2, 6, 4, 10, 7];
+
+// filteredModula = modulaNums.filter((num) => {
+//   return num > 5;
+// });
+
+// function moduleNum(arr) {
+
+// }
+
+// 9.
+
+filterLower = nums.filter((num) => {
+  return num < 3;
+});
+
+reduceLower = filterLower.reduce((a, b) => {
+  return a * b;
+});
+
+console.log("9.", reduceLower);
+
+// 10.
+
+const lowerNums = [1, 2, 3, 6, 7];
+
+const filterLowerFive = lowerNums.filter((num) => {
+  return num < 5;
+});
+
+function lowerFive(arr) {
+  return arr ** 2;
+}
+
+squareLower = filterLowerFive.map(lowerFive);
+reduceLowerFive = squareLower.reduce((a, b) => {
+  return a + b;
+});
+
+console.log("10.", reduceLowerFive);
+
+// 11.
+
+const higherThanTen = [5, 12, 8, 20, 3];
+
+filterhigherThanTen = higherThanTen.filter((num) => {
+  return num > 10;
+});
+
+function higherTen(arr) {
+  return arr ** 2;
+}
+
+squareHigherTen = filterhigherThanTen.map(higherTen);
+reduceHigherTen = squareHigherTen.reduce((a, b) => {
+  return a + b;
+});
+
+console.log("11.", reduceHigherTen);
+
+// 12.
+
+const tripleNum = [1, 2, 3, 4, 5, 6]
+  .filter((num) => num % 2 === 0)
+  .map((num) => num ** 3)
+  .reduce((a, b) => a + b);
+
+console.log("12.", tripleNum);
+
+// 13.
+
+const dividThreeNums = [3, 4, 6, 7, 9]
+  .filter((num) => num % 3 === 0)
+  .map((num) => num ** 3)
+  .reduce((a, b) => a + b);
+
+console.log("13.", dividThreeNums);
+
+// 14.
+
+const higherZeroNums = [-5, 0, 4, 7, -1]
+  .filter((num) => num > 0)
+  .map((num) => num ** 2)
+  .reduce((a, b) => a + b);
+
+console.log("14.", higherZeroNums);
+
+// 15.
+
+const higherTenNums = [8, 12, 15, 5]
+  .filter((num) => num > 10)
+  .map((num) => num * 2)
+  .reduce((a, b) => a + b);
+
+console.log("15.", higherTenNums);
+
+// 16.
+
+const modulaThree = [2, 6, 4, 10, 7];
+
+filteredmodulaThree = modulaThree.filter((num) => {
+  return num > 5;
+});
+
+function modulaThreeFunc(arr) {
+  return arr % 3;
+}
+
+modulaThreeFunction = filteredmodulaThree.map(modulaThreeFunc);
+reducemodulaThree = modulaThreeFunction.reduce((a, b) => {
+  return a + b;
+});
+
+console.log("16.", reducemodulaThree);
+
+// 17.
+
+const lowerFiveModule = [1, 2, 3, 6, 7]
+  .filter((num) => num < 5)
+  .map((num) => num % 2)
+  .reduce((a, b) => a + b);
+
+console.log("17.", lowerFiveModule);
+
+// 18.
+
+const cdt = [3, 4, 6, 7, 9];
+
+filterCdt = cdt.filter((num) => {
+  return num % 3 === 0;
+});
+
+function cdtFunc(arr) {
+  return arr ** 2;
+}
+
+cdtFunction = filterCdt.map(cdtFunc);
+reduceCdt = cdtFunction.reduce((a, b) => {
+  return (a + b) % 10;
+});
+
+console.log("18.", reduceCdt);
+
+// 19.
+
+const htm = [5, 12, 8, 20, 3]
+  .filter((num) => num > 10)
+  .map((num) => num ** 2)
+  .reduce((a, b) => (a + b) % 7);
+
+console.log("19.", htm);
+
+// 20.
+
+const words = ["hi", "hello", "hey", "world", "javascript"];
+
+filterWords = words.filter((word) => {
+  return word.length >= 5;
+});
+
+function wordFunc(str) {
+  return str.length;
+}
+
+wordFunction = filterWords.map(wordFunc);
+reduceWord = wordFunction.reduce((a, b) => {
+  return a + b;
+});
+
+console.log("20.", reduceWord);
